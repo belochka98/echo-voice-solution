9fab7ef88350d677655cb0a84b2e8aef07dd5d90
sviridov_v <vsviridov@vsegda-da.com>
2023-02-26 16:10:28 +0300

    [NO-TASK] - fix sh script for postgres init docker && configs

---
+++ docker/configuration/postgres/script/initExtensions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+create_user_with_db() {
+    echo "  Try to create database: '$1' with owner: '$2'"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $2 WITH ENCRYPTED PASSWORD '$2';
+    CREATE DATABASE $1 OWNER $2;
+EOSQL
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$1" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+EOSQL
+    echo "  Finished creating with exit code $?"
+}
+
+create_user_with_db 'user_db' 'evoice_user_db'
+create_user_with_db 'keycloak_db' 'evoice_keycloak_db'
