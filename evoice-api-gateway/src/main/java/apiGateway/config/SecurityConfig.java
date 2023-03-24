7c3f1070a81d3d7839df21e9a516d47f13d09b3e
Sviridov Vlad <vsviridov@vsegda-da.com>
2023-03-25 00:03:58 +0300

    [NO-TASK] - fix grafana

--- evoice-api-gateway/src/main/java/apiGateway/config/SecurityConfig.java
+++ evoice-api-gateway/src/main/java/apiGateway/config/SecurityConfig.java
@@ -2,20 +2,58 @@ package apiGateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(
+    @Order(1)
+    public SecurityWebFilterChain securityAnyExchange(
             ServerHttpSecurity serverHttpSecurity
     ) {
         serverHttpSecurity
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/api/**"))
                 .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return serverHttpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityWebFilterChain securityActuator(
+            ServerHttpSecurity serverHttpSecurity,
+            ReactiveAuthenticationManager reactiveAuthenticationManager
+    ) {
+        serverHttpSecurity
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/actuator/**"))
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .authenticationManager(reactiveAuthenticationManager)
+                .httpBasic();
+        return serverHttpSecurity.build();
+    }
+
+    @Bean
+    ReactiveAuthenticationManager reactiveAuthenticationManager() {
+        return new UserDetailsRepositoryReactiveAuthenticationManager(getInMemoryUserDetails());
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService getInMemoryUserDetails() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(admin);
     }
 }
